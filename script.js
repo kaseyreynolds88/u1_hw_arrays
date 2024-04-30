6a3f6052d6e7577036a182431ebfb60a7fed228e
Kasey Reynolds <reynolds-8@hotmail.com>
2024-04-30 17:21:35 -0400

    completed prompt 2

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ Exercise 1: Define an empty array
 Exercise 1 has been completed for you:
 */
 
-const foods = [];  
+// const foods = [];  
 
-console.log('Exercise 1 result:', foods);
+// console.log('Exercise 1 result:', foods);
 
 
 /*
@@ -21,9 +21,8 @@ Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 Complete Exercise 2 in the space below:
 */
 
-
-
-console.log('Exercise 2 result:', foods);
+const foods = ["pizza", "cheeseburger"]
+console.log(foods);
 
 
 /*
@@ -36,114 +35,114 @@ Complete Exercise 3 in the space below:
 
 
 
-console.log('Exercise 3 result:', foods);
+// console.log('Exercise 3 result:', foods);
 
 
-/*
-Exercise 4: Access an array element
+// /*
+// Exercise 4: Access an array element
 
-1) Retrieve the 'pizza' string from the array based on its position (index) in
-   the array.  
+// 1) Retrieve the 'pizza' string from the array based on its position (index) in
+//    the array.  
 
-2) Assign it to a variable called `favFood`.
+// 2) Assign it to a variable called `favFood`.
 
-Complete Exercise 4 in the space below:
-*/
-
-
-
-console.log('Exercise 4 result:', favFood);
+// Complete Exercise 4 in the space below:
+// */
 
 
 
-/*
-Exercise 5: Insert an element between two others
-
-1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
-
-Complete Exercise 5 in the space below:
-*/
+// console.log('Exercise 4 result:', favFood);
 
 
 
-console.log('Exercise 5 result:', foods);
+// /*
+// Exercise 5: Insert an element between two others
 
+// 1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
 
-/*
-Exercise 6: Replace elements
-
-1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
-
-Complete Exercise 6 in the space below:
-*/
+// Complete Exercise 5 in the space below:
+// */
 
 
 
-console.log('Exercise 6 result:', foods)
+// console.log('Exercise 5 result:', foods);
+
+
+// /*
+// Exercise 6: Replace elements
+
+// 1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+// Complete Exercise 6 in the space below:
+// */
 
 
 
-/*
-Exercise 7: Using the `slice()` method
-
-1) Use the `slice()` method to create a new array that contains 'sushi' and 
-   'cupcake'.
-
-2) Assign it to a variable named `yummy`.
-
-Complete Exercise 7 in the space below:
-*/
-
-
-console.log('Exercise 7 result:', yummy);
-
-
-/*
-Exercise 8: Finding an index
-
-1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
-   `foods` array. 
-
-2) Assign it to a variable named `soyIdx`.
-
-Complete Exercise 8 in the space below:
-*/
+// console.log('Exercise 6 result:', foods)
 
 
 
-console.log('Exercise 8 result:', soyIdx);
+// /*
+// Exercise 7: Using the `slice()` method
+
+// 1) Use the `slice()` method to create a new array that contains 'sushi' and 
+//    'cupcake'.
+
+// 2) Assign it to a variable named `yummy`.
+
+// Complete Exercise 7 in the space below:
+// */
 
 
-/*
-Exercise 9: Joining elements
-
-1) Use the `join()` method to concatenate the strings in the `foods` array, 
-   separated by ' -> '. 
-
-2) Assign the result to a variable called `allFoods`. 
-
-Note: The final result should log as:
-'taco -> sushi -> cupcake -> tofu -> cheeseburger'
-
-Complete Exercise 9 in the space below:
-*/
+// console.log('Exercise 7 result:', yummy);
 
 
+// /*
+// Exercise 8: Finding an index
 
-console.log('Exercise 9 result:', allFoods);
+// 1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+//    `foods` array. 
 
+// 2) Assign it to a variable named `soyIdx`.
 
-/*
-Exercise 10: Check for an element
-
-1) Using the .includes() method, check if the `foods` array contains the string
-   'soup'.
-
-2) Assign the result to a variable called `hasSoup``.
-
-Complete Exercise 10 in the space below:
-*/
+// Complete Exercise 8 in the space below:
+// */
 
 
 
-console.log('Exercise 10 result:', hasSoup);
+// console.log('Exercise 8 result:', soyIdx);
+
+
+// /*
+// Exercise 9: Joining elements
+
+// 1) Use the `join()` method to concatenate the strings in the `foods` array, 
+//    separated by ' -> '. 
+
+// 2) Assign the result to a variable called `allFoods`. 
+
+// Note: The final result should log as:
+// 'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+// Complete Exercise 9 in the space below:
+// */
+
+
+
+// console.log('Exercise 9 result:', allFoods);
+
+
+// /*
+// Exercise 10: Check for an element
+
+// 1) Using the .includes() method, check if the `foods` array contains the string
+//    'soup'.
+
+// 2) Assign the result to a variable called `hasSoup``.
+
+// Complete Exercise 10 in the space below:
+// */
+
+
+
+// console.log('Exercise 10 result:', hasSoup);
